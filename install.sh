228feb8d12f2b8c429d57236392a47a7985e33ab
Grisuno <grisuno@gmail.com>
2025-11-01 19:59:01 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-python3 -m venv env
-source env/bin/activate
-pip install -r requirements.txt
+arm-linux-gnueabihf-gcc -I. -I/home/grisun0/curl-arm32/include -I/home/grisun0/openssl-armhf/include -I/home/grisun0/zlib-armhf/include beacon.c aes.c cJSON.c -L/home/grisun0/curl-arm32/lib -L/home/grisun0/openssl-armhf/lib -L/home/grisun0/zlib-armhf/lib -lcurl -lssl -lcrypto -lz -ldl -lpthread -o beacon
+
